1ed22a43f3014fcf293a519438f719f7a63710c9
SDLucasFerrenti <105218980+SDLucasFerrenti@users.noreply.github.com>
2023-05-30 04:27:32 -0300

    repos-list

--- js/github-list.js
+++ js/github-list.js
@@ -1,11 +1,11 @@
 var repos = [];
 
-$.get("api.github.com/users/LucasFerrenti/repos", function(data){
+$.get("https://api.github.com/users/LucasFerrenti/repos", function(data){
     repos = data;
     console.log(data);
     var list = $("#git-list");
 
     data.forEach(element => {
-        list.append( "<strong>" + element.name +"</strong>" );
+        list.append( "<li>" + element.name +"</li>" );
     });
 });
